3168b29c24d4e2348e6f7c8c07a3b9eb551d2b63
Gabriel Torres <biel.rodrigues7@gmail.com>
2025-05-06 11:25:04 -0300

    pequeno uptade 1.2

--- Assets/Scripts/ShrineOut.cs
+++ Assets/Scripts/ShrineOut.cs
@@ -22,7 +22,7 @@ public class ShrineOut : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            SceneManager.LoadScene("Land");
+            SceneManager.LoadScene("terrain cena");
         }
     }
 }
