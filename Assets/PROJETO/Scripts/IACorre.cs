178541eca357dbab13fe277589a694a348a5972b
Gabriel Torres <biel.rodrigues7@gmail.com>
2025-05-13 12:27:43 -0300

    começo das IAs
    ia que foge implantada, faltam ajustes.

--- Assets/PROJETO/Scripts/IACorre.cs
+++ Assets/PROJETO/Scripts/IACorre.cs
@@ -3,17 +3,52 @@ using UnityEngine.AI;
 
 public class IACorre : MonoBehaviour
 {
-    NavMeshAgent agent;
-    
+   public NavMeshAgent agent ;  
+   public GameObject chaser ;
+    [SerializeField] private float disPos;
     
     void Start()
     {
-        
+        if(!chaser)
+        {
+           chaser = GameObject.FindGameObjectWithTag("Player");
+        }
+        agent.destination = RandomPosition(100);
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+       
+         
+      
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("cahsing");
+            agent.speed = 10;
+            Vector3 normDir = (chaser.transform.position - transform.position).normalized;
+            normDir = Quaternion.AngleAxis(Random.Range(0, 170), Vector3.up) * normDir;
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        agent.speed = 3;
+        agent.destination = RandomPosition(100);
+    }
+
+
+
+    Vector3 RandomPosition(float range)
+    {
+        Vector3 pos;
+        pos = transform.position + new Vector3(UnityEngine.Random.Range(-range, range)
+            , 0
+            , UnityEngine.Random.Range(-range, range));
+        return pos;
+    }
+
 }
