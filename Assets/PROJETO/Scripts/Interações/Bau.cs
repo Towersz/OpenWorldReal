84cb40c72bfae53804423009f7f7032bbdd10831
Gabriel Torres <biel.rodrigues7@gmail.com>
2025-06-16 10:26:05 -0300

    Vulcão
    particula do vulcão e bau adcionado.
    
    Baú feitor por joão minami

--- Assets/PROJETO/Scripts/Interações/Bau.cs
+++ Assets/PROJETO/Scripts/Interações/Bau.cs
@@ -3,10 +3,12 @@ using UnityEngine;
 public class Bau : MonoBehaviour, IInteractable
 {
     [SerializeField] private string _prompt;
+    public Animator anim;
     public string InteractionPrompt => _prompt;
 
     public bool Interact(Interactor interactor)
     {
+        
         Debug.Log(message: "abrir bau!");
             return true;
     }
